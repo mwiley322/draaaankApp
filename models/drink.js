879e87adade4337e3bba2588541af650f454f7d4
emamiali <emamiali1993@gmail.com>
2017-03-23 11:40:03 -0700

    modifed the drinks Schema

--- models/drink.js
+++ models/drink.js
@@ -3,10 +3,11 @@ var Schema = mongoose.Schema;
 
 var DrinkSchema = new Schema({
   name : String,
-  sevingSize : Number,
   img : String,
   ingredients : [ String ],
-  origin : String
+  origin : String,
+  glass : String,
+  garnish : String
 });
 
 var DrinkSchema = mongoose.model('Drink', DrinkSchema);
